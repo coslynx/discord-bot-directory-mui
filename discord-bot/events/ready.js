591e40623afe31bde69a0e74928364aff902d34b
CosLynx AI <drixisgod@gmail.com>
2024-11-29 00:49:59 +0530

    generated file: `discord-bot/events/ready.js`

---
+++ discord-bot/events/ready.js
@@ -0,0 +1,9 @@
+const { Client, GatewayIntentBits } = require('discord.js');
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.login('YOUR_BOT_TOKEN');
+```
